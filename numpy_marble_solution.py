705d1d7ae9599126f55ee6c98453253ba0747be6
Yihsuan Liao <vivianyihsuan@gmail.com>
2020-10-03 13:25:28 -0400

    update function 3 and doctest

--- numpy_marble_solution.py
+++ numpy_marble_solution.py
@@ -152,69 +152,37 @@ def is_stable(sculpture: np.ndarray) -> bool:
     given, whether it will sit stably upon its base.
 
     :param sculpture: NDarray representing a sculpture of variable density material.
+    >>>is_stable(np.array([[[1,2], [3,4]]]))
+    True
+    >>>is_stable(np.array([[[1,np.nan,np.nan], [np.nan,np.nan,6],[np.nan, np.nan, np.nan]]]))
+    False
+
     """
     # TODO: Complete this function.
     # TODO: Add a few good, working Doctests
 
-    ## 假設sculpture已經是ndarray (scuplture = np.array().reshape())
-    ## if value is nAn -> needs to replace 0 so that center of mass works
-    #sculpture = np.nan_to_num(sculpture, nan = 0)  ## sculpture是不是要從上一個function拿下來啊？
-    #center = center_of_mass(sculpture)
-
-    # use convexhull把3d轉成2d 看center of mass有沒有在base裡面
-    #ch = ConvexHull(points=sculpture, incremental=True) ## 2d或3d應該都可以用convex hull? ## 這裡還沒弄完 因為上面sculpture的地方還沒出來
-    #sculpture_base =   ## 這個應該要是二維
-    #print("ch")
-
-
-    #if center in sculpture_base:
-    #    print("Stable")  # 還要看需不需要傳進dict裡面
-    #else:
-    #    print("Unstable")
-
-
-    # output #還要調一下固定寬度 跟tab一格
-    #print("Shape File: ", 傳進shapefile)
-    #print("Block File:", 傳進blockfile )
-    #print("Rotation: {} Mean density: {} {}".format(Rotation, Mean Density, is_stable?))
-    #return
-
-
-
-
-    # sculpture_base = sculpture[0, :, :]
-
-    # ch1 = ConvexHull(points=sculpture_base, incremental=True)
-    # print("ch1:")
-    # summarize_hull_properties
-
-    ##我是分割线——————————————————————————————————————————————————————————
-
     sculpture = carve_sculpture_from_density_block(shape_1, marble_block_1)
     sculpture = np.nan_to_num(sculpture, 0)  # convert nan to zero to make center_of_mass work
     center = center_of_mass(sculpture)
-    if center > 2:
+    if len(center) > 2:
         center = center[1:]
 
     ## slice the bottom layer ( last item in the array)
-    sculpture_base = sculpture[-1]  # 3d -> 2d -> result a sqare
+    sculpture_base = sculpture[-1]  # 3d -> 2d -> result a square
     # nan arrays
-    sculpture_base = np.argwhere(sculpture_base > 0)  # return array of points
+    sculpture_base = np.argwhere(sculpture_base)  # return array of points
     # np.transpose(sculpture_base) (?)
 
-    # use convexhull 看center of mass有沒有在base裡面
-    # get ch1
+    # use convexhull to calculate area and check if its stable or not
+    # get area1
     hull = ConvexHull(sculpture_base, incremental=True)
     area1 = hull.area
 
     # add points to calculate new area
-    # arrays with new points
     hull.add_points(center)
-
-    #area after adding center points to hull
+    # area2 -> after adding center points to hull
     area2 = hull.area
-
-    # compare ch1 and ch2, if ch1 == ch2 return True, else False
+    # compare area1 and area2, if ch1 == ch2 return True, else False
     if area1 == area2:
         return True
     else:
