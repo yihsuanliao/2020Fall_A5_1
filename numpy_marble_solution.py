feeba0f081cec645cf7d9f83c5536e72890e0ee2
Yihsuan Liao <vivianyihsuan@gmail.com>
2020-10-03 23:29:23 -0400

    finish function 3 sculpture

--- numpy_marble_solution.py
+++ numpy_marble_solution.py
@@ -152,35 +152,32 @@ def is_stable(sculpture: np.ndarray) -> bool:
     given, whether it will sit stably upon its base.
 
     :param sculpture: NDarray representing a sculpture of variable density material.
-
-     >>>is_stable(np.array([[[1,2], [3,4]]]))
+    >>> is_stable(np.array([[[1,2], [4,5]]]))
     True
-    >>>is_stable(np.array([[[1,np.nan,np.nan], [np.nan,np.nan,6],[np.nan, np.nan, np.nan]]]))
-    False
+    >>> is_stable(np.array([[[1, np.nan, np.nan], [np.nan, np.nan, 6], [np.nan, np.nan, np.nan], [1, np.nan, np.nan]]]))
+    True
+
     """
-    # TODO: Complete this function.
-    # TODO: Add a few good, working Doctests
-
     sculpture = np.nan_to_num(sculpture, 0)  # convert nan to zero to make center_of_mass work
-    center = center_of_mass(sculpture)
-    if len(center) > 2:
-        center = center[1:]
+    center = list(center_of_mass(sculpture)) # (z, y, x)
 
-    ## slice the bottom layer ( last item in the array)
-    sculpture_base = sculpture[-1]  # 3d -> 2d -> result a square
+    # slice the bottom layer ( last item in the array) 3d -> 2d -> result a square
+    sculpture_base = sculpture[-1]
     # nan arrays
-    sculpture_base = np.argwhere(sculpture_base)  # return array of points
+    sculpture_base = np.argwhere(sculpture_base).tolist()  # return array of points
     # np.transpose(sculpture_base) (?)
 
     # use convexhull to calculate area and check if its stable or not
     # get area1
-    hull = ConvexHull(sculpture_base, incremental=True)
+    hull = ConvexHull(points=sculpture_base, incremental=True)
     area1 = hull.area
 
+    del center[0]
     # add points to calculate new area
-    hull.add_points(center)
+    sculpture_base.append(center)
     # area2 -> after adding center points to hull
-    area2 = hull.area
+    hull2 = ConvexHull(points=sculpture_base, incremental=True)
+    area2 = hull2.area
     # compare area1 and area2, if ch1 == ch2 return True, else False
     if area1 == area2:
         return True
